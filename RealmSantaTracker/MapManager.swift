3776ab79db673b0ef5645e56188440f9b6a584bc
Keita Ito <keitaitok@gmail.com>
2016-12-24 23:50:26 -0800

    Add Santa's route rendering feature to mapManager

--- RealmSantaTracker/MapManager.swift
+++ RealmSantaTracker/MapManager.swift
@@ -10,20 +10,43 @@ import UIKit
 import MapKit
 
 class MapManager: NSObject {
+    private let mapView: MKMapView
     private let santaAnnotation = MKPointAnnotation()
+    private var routeOverlay: MKPolyline
     
     init(mapView: MKMapView) {
+        self.mapView = mapView
         santaAnnotation.title = "🎅"
+        routeOverlay = MKPolyline(coordinates: [], count: 0)
         super.init()
         mapView.addAnnotation(self.santaAnnotation)
+        self.mapView.delegate = self
     }
     
     func update(with santa: Santa) {
         // Update the map to show Santa's new location
         let santaLocation = santa.currentLocation.clLocationCoordinate2D
+        let coordinates: [CLLocationCoordinate2D] = santa.route.flatMap { $0.location?.clLocationCoordinate2D }
         DispatchQueue.main.async {
             self.santaAnnotation.coordinate = santaLocation
+            self.mapView.remove(self.routeOverlay)
+            self.routeOverlay = MKPolyline(coordinates: coordinates, count: coordinates.count)
+            self.mapView.add(self.routeOverlay)
         }
+    }
+}
+
+extension MapManager: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard overlay is MKPolyline else {
+            return MKOverlayRenderer(overlay: overlay)
+        }
+        
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = .black
+        renderer.lineWidth = 3
+        renderer.lineDashPattern = [3, 6]
+        return renderer
     }
 }
 
