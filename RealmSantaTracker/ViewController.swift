3bc02e48f0b1f4032f2284d980fa7719c7a72d0b
Keita Ito <keitaitok@gmail.com>
2016-12-24 19:20:04 -0800

    Initial Commit

---
+++ RealmSantaTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmSantaTracker
+//
+//  Created by Keita Ito on 12/24/16.
+//  Copyright © 2016 Keita Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
